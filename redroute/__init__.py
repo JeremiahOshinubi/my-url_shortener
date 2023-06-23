30323cad905f66829f1d75133277bdacf2b8b53a
jerry <feranmijerry2001@gmail.com>
2023-06-23 18:56:36 +0100

    more changes

--- redroute/__init__.py
+++ redroute/__init__.py
@@ -10,8 +10,15 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+# Set the default value for SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+
+# Check if the DATABASE_URL environment variable is set
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    # Replace the URL scheme to use "ql://" instead of "://"
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url.replace("://", "ql://", 1)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
@@ -23,12 +30,10 @@ app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 app.config['CACHE_TYPE'] = 'SimpleCache'
 app.config['CACHE_DEFAULT_TIMEOUT'] = 300
 
-
 db = SQLAlchemy(app)
 mail = Mail(app)
 share = Share(app)
 cache = Cache(app)
-
 
 login_manager = LoginManager()
 login_manager.login_view = 'login'
